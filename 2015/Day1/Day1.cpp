49bd427ce42c4784ded41cf4818114fb475492a6
LimonHead <lukejoylimon@gmail.com>
2024-06-21 22:32:46 +0100

    Day 1 Part 1

--- 2015/Day1/Day1.cpp
+++ 2015/Day1/Day1.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string input{};
+    std::ifstream input_file("input.txt");
+    std::getline(input_file, input);
+
+    // Part 1 //
+
+    int floor{ 0 };
+    for (char c : input)
+    {
+        if (c == '(') floor++;
+        else floor--;
+    }
+
+    std::cout << floor;
+    return 0;
 }
