37b4d0804a924baf35e1c23a3c1c7c8edee1c205
LimonHead <lukejoylimon@gmail.com>
2024-06-21 22:45:03 +0100

    day 1 part 2

--- 2015/Day1/Day1.cpp
+++ 2015/Day1/Day1.cpp
@@ -8,15 +8,38 @@ int main()
     std::ifstream input_file("input.txt");
     std::getline(input_file, input);
 
+    
+
     // Part 1 //
+
+    int result1{ 0 };
+
+    for (char c : input)
+    {
+        if (c == '(') result1++;
+        else result1--;
+    }
+
+    std::cout << "Part 1: " << result1 << '\n';
+
+
+
+    // Part 2 //
+
+    int result2{ 0 };
 
     int floor{ 0 };
     for (char c : input)
     {
+        result2++;
         if (c == '(') floor++;
         else floor--;
+        if (floor < 0) break;
     }
 
-    std::cout << floor;
+    std::cout << "Part 1: " << result2 << '\n';
+
+
+
     return 0;
 }
