a9c990ff22857efe2fd8b754101eeb197dcba8e9
LimonHead <lukejoylimon@gmail.com>
2024-06-23 14:20:54 +0100

    day 2 part 1

---
+++ 2015/Day2/Day2.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+std::vector<std::string> ParseByDelimiter(std::string input, char delimiter)
+{
+	std::vector<std::string> output{};
+	std::string temp{};
+
+	for (char c : input)
+	{
+		if (c == delimiter)
+		{
+			output.push_back(temp);
+			temp = "";
+		}
+		else
+		{
+			temp += c;
+		}
+	}
+	output.push_back(temp);
+
+	return output;
+}
+
+void PrintVector(std::vector<int> vector)
+{
+	for (int item : vector)
+	{
+		std::cout << item << ",\n";
+	}
+}
+void PrintVector(std::vector<std::string> vector)
+{
+	for (std::string item : vector)
+	{
+		std::cout << item << ",\n";
+	}
+}
+
+int main()
+{
+	std::vector<std::string> input{};
+	std::ifstream input_file("input.txt");
+	std::string temp{};
+
+	while (std::getline(input_file, temp))
+	{
+		input.push_back(temp);
+	}
+
+
+
+	// Part 1 //
+
+	int result_1{ 0 };
+
+	for (std::string s : input)
+	{
+		std::vector<std::string> split_string{ ParseByDelimiter(s, 'x') };
+		std::vector<int> dimensions{ std::stoi(split_string[0]), std::stoi(split_string[1]), std::stoi(split_string[2]) };
+		std::vector<int> areas{ dimensions[0] * dimensions[1],
+								dimensions[0] * dimensions[2],
+								dimensions[1] * dimensions[2] };
+
+		result_1 += 2 * areas[0] + 2 * areas[1] + 2 * areas[2] + *std::min_element(areas.begin(), areas.end());
+	}
+
+	std::cout << "Part 1: " << result_1 << '\n';
+
+
+
+	// Part 2 //
+
+	int result_2{ 0 };
+
+
+
+	std::cout << "Part 2: " << result_2 << '\n';
+
+
+
+	return 0;
+}
